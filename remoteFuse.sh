a31265e961c873798ff9910f28b318c9de15520e
tlambert03 <tlambert03@gmail.com>
2016-05-18 11:34:24 -0400

    Update remoteFuse.sh

--- remoteFuse.sh
+++ remoteFuse.sh
@@ -4,6 +4,9 @@
 # it can be used as follows:
 # remoteFuse /path/to/parent/directory 0.5
 
+# note, to prevent the need for passwords each time, set up SSH keys.
+# https://help.github.com/articles/generating-an-ssh-key/
+
 INPUT=$1 # first argument is the input directory (the one with SPIMA and SPIMB in it)
 ZSTEP=$2 # second argument is Z step
 [[ ${ZSTEP} ]] || ZSTEP=0.5 # second argument is optional, and will default to this value
@@ -15,7 +18,7 @@ HOST=10.10.10.10 # host ip address
 DEST=/destination/directory/ # directory on host to copy files to
 
 # if you routinely use certain parameters, you can adjust this script here
-SCRIPT="/home/tjl10/makeMIPAVjobs.sh -n 4 -f 1 -z $ZSTEP"
+SCRIPT="/path/to/makeMIPAVjobs.sh -n 4 -f 1 -z $ZSTEP"
 
 rsync -rzv $INPUT $USER@$HOST:$DEST
 
